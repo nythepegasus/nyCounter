c741795a5665f0c0bdc77a2f9b1f554bbb372378
ny <mac@nythepegas.us>
2024-10-09 23:06:36 -0400

    Actually fix automatic test notes

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
-#  ci_post_clone.sh
+#  ci_post_xcodebuild.sh
 
 git fetch --tags --deepen 3
 if [[ $? -ne 0 ]]; then
@@ -7,7 +7,7 @@ echo "Git fetch failed."
 exit 1
 fi
 # Get commit messages from the last tag to the current HEAD
-git log $(git describe --tags --abbrev=0)..HEAD --pretty=format:"%s" > TestFlight/WhatToTest.en-US.txt
+git log $(git describe --tags --abbrev=0)..HEAD --pretty=format:"%s" > ../TestFlight/WhatToTest.en-US.txt
 if [[ $? -ne 0 ]]; then
 echo "Failed to write commit messages to WhatToTest.en-US.txt."
 exit 1
